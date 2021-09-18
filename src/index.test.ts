614c85bada1da60b995dc2f080a7fc8e1ba57e94
John B <john@nexys.ch>
2021-09-18 13:12:53 +0200

    init

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,8 @@
-test('dummy', () => {
-  expect(true).toEqual(true);
+import * as I from "./index";
+
+test("get IP from hostname", async () => {
+  expect(await I.getIPFromHostName("example.org")).toEqual({
+    address: "93.184.216.34",
+    family: 4,
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('main entry point');
+import dns from "dns";
+
+/**
+ *
+ * @param hostname
+ * @see https://nodejs.org/api/dns.html
+ * @returns {address: ip, family: IP version, ie IPv4}
+ */
+export const getIPFromHostName = (
+  hostname: string
+): Promise<{ address: string; family: number }> =>
+  new Promise((resolve, reject) => {
+    dns.lookup(hostname, (err, address, family) => {
+      if (err) {
+        reject(err);
+      }
+
+      resolve({ address, family });
+    });
+  });
